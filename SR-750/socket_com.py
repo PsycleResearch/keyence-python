679cbde21dce48b499da6721b5956f4f839bdee6
Quentiin93 <quentin.heranval@sfr.fr>
2020-12-01 13:51:35 +0100

    Update for SR-750

--- SR-750/socket_com.py
+++ SR-750/socket_com.py
@@ -1,9 +1,12 @@
+import time
 import socket                
   
 s = socket.socket()          
-s.connect(('192.168.0.100', 9004)) 
+s.connect(('192.168.1.93', 9004)) 
 s.settimeout(1000)
 s.send(b"LON\r")
+time.sleep(1)
+s.send(b"LOFF\r")
 
 print(s.recv(1024).decode('utf-8'))
 s.close()
